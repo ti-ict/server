ab855744632c7b782fb27dbbfddfeb94e7a16ddc
LordImmaculate <101836390+LordImmaculate@users.noreply.github.com>
2026-06-01 11:08:16 +0200

    fix: use tenant ID

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -8,13 +8,10 @@ export const auth = betterAuth({
     provider: "postgresql"
   }),
   socialProviders: {
-    github: {
-      clientId: process.env.GITHUB_CLIENT_ID!,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET!
-    },
     microsoft: {
       clientId: process.env.MICROSOFT_CLIENT_ID!,
-      clientSecret: process.env.MICROSOFT_CLIENT_SECRET!
+      clientSecret: process.env.MICROSOFT_CLIENT_SECRET!,
+      tenantId: process.env.MICROSOFT_TENANT_ID!
     }
   },
   plugins: [admin()]
